e584962298ee22f899cecf5bc4d382db5fb93335
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-01-05 18:08:34 +0300

    fixed problems css and connect static files with template

---
+++ webapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from webapp.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+app_name = 'webapp'
+
+urlpatterns = [
+    path('', index, name='home'),
+    ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
