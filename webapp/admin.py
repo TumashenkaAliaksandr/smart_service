4ede436185c419bbf2f46f54ef25b4d6c7cd4533
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-02-12 21:30:35 +0300

    added new model Favors

--- webapp/admin.py
+++ webapp/admin.py
@@ -29,3 +29,12 @@ class Shop_EncodersAdmin(admin.ModelAdmin):
         return obj.image.url if obj.image else None  # Отображение превью изображения в админке
 
     image.short_description = 'Photo Preview'  # Название столбца с превью изображения
+
+@admin.register(Favors)
+class FavorsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'description_two', 'image', 'is_main', 'link', 'price')  # Отображаемые поля в списке брэндов
+
+    def image(self, obj):
+        return obj.image.url if obj.image else None  # Отображение превью изображения в админке
+
+    image.short_description = 'Photo Preview'  # Название столбца с превью изображения
--- webapp/models.py
+++ webapp/models.py
@@ -2,6 +2,24 @@ from django.db import models
 
 
 
+class Favors(models.Model):
+    """Favors"""
+    name = models.CharField(max_length=100, verbose_name='name')
+    description = models.TextField(verbose_name='description')
+    description_two = models.TextField(verbose_name='description_two', default='Описание')
+    image = models.ImageField(upload_to='services', verbose_name='photo')
+    is_main = models.BooleanField(default=False)
+    link = models.URLField()  # Поле для хранения ссылки
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price', default=0.00)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Favors"
+        verbose_name_plural = "Favors"
+
+
 class Services(models.Model):
     """Services model"""
     name = models.CharField(max_length=100, verbose_name='name')
@@ -18,7 +36,6 @@ class Services(models.Model):
     class Meta:
         verbose_name = "Services"
         verbose_name_plural = "Services"
-
 
 class Shop_Encoders(models.Model):
     """Services model"""
