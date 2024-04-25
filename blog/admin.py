99e2930ec63fcd6130bcefb83f94aaf339bbcf3e
TumashenkaAliaksandr <Badminton500@inbox.lv>
2024-04-25 11:47:58 +0300

    Added the BlogNews model connection to the blog and single blog templates

--- blog/admin.py
+++ blog/admin.py
@@ -10,6 +10,7 @@ class BlogNewsAdminForm(forms.ModelForm):
         fields = '__all__'
         widgets = {
             'description': CKEditorWidget(),
+            'description_company': CKEditorWidget(),
             'description_small': CKEditorWidget(),
         }
 
@@ -17,7 +18,8 @@ class BlogNewsAdminForm(forms.ModelForm):
 @admin.register(BlogNews)
 class BlogNewsAdmin(admin.ModelAdmin):
     form = BlogNewsAdminForm
-    list_display = ('title', 'description_small', 'location', 'photo', 'pub_date', 'author', 'comment_author')
+    list_display = ('title', 'description_small', 'description', 'description_company', 'location', 'photo', 'logo_photo',
+                    'pub_date', 'author', 'comment_author')
 
     def author(self, obj):
         return obj.author.username if obj.author else '-'
--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,12 @@ from django.conf import settings
 
 class BlogNews(models.Model):
     title = models.CharField(max_length=200, verbose_name='Name')
-    description_small = models.TextField(max_length=400, default='', verbose_name='Description_small')
-    description = models.TextField(verbose_name='Description')
+    description_small = models.TextField(max_length=500, default='', verbose_name='Description_small')
+    description = models.TextField(verbose_name='Description', default='')
+    description_company = models.TextField(verbose_name='Description_company', default='Описание для нижнего блока')
     location = models.CharField(max_length=200, verbose_name='Location', default='')
     photo = models.ImageField(upload_to='news_photos/', null=True, blank=True, verbose_name='Photo')
+    logo_photo = models.ImageField(upload_to='news_photos/', null=True, blank=True, verbose_name='Logo Photo')
     pub_date = models.DateTimeField(verbose_name='Publication Date')  # Added publication date field
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Author', related_name='news', null=True, blank=True)
     author_photo = models.ImageField(upload_to='author_photos/', null=True, blank=True, verbose_name='Author Photo')
